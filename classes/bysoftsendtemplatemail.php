79557efdaa48e14d58a63a91ed9b964e08e6a180
altsee <info@example.com>
2010-11-01 07:33:26 +0000

    add cc and bcc
    git-svn-id: http://svn.projects.ez.no/sendtplmail@3 07101de3-f570-419e-b2a4-a7bda63e94ca

--- classes/bysoftsendtemplatemail.php
+++ classes/bysoftsendtemplatemail.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * Send Email by using template arguments
+ * 
+ * @author cavin.deng
+ * @since 2010-10-30
+ * 
+ */
 class bysoftSendTemplateMail
 {
     const SEND_TPL_MAIL = 'send_tpl_mail';
@@ -35,26 +41,19 @@ class bysoftSendTemplateMail
         switch ($functionName) {
             case self::SEND_TPL_MAIL:
                 
+                // 1.1 fetch arguments from template
                 $sender = $this->_getParam('sender', $tpl, $rootNamespace, $currentNamespace, $functionParameters, $functionPlacement);
                 $receivers = $this->_getParam('receivers', $tpl, $rootNamespace, $currentNamespace, $functionParameters, $functionPlacement);
                 $ccReceivers = $this->_getParam('cc_receivers', $tpl, $rootNamespace, $currentNamespace, $functionParameters, $functionPlacement);
+                $bccReceivers = $this->_getParam('bcc_receivers', $tpl, $rootNamespace, $currentNamespace, $functionParameters, $functionPlacement);
                 $subject = $this->_getParam('subject', $tpl, $rootNamespace, $currentNamespace, $functionParameters, $functionPlacement);
                 $body = $this->_getParam('body', $tpl, $rootNamespace, $currentNamespace, $functionParameters, $functionPlacement);
 				$contentType = $this->_getParam('content_type', $tpl, $rootNamespace, $currentNamespace, $functionParameters, $functionPlacement);
                 
-                $sender = $this->_getParam('sender', $tpl, $rootNamespace, $currentNamespace, $functionParameters, $functionPlacement);
-                
-                // bcc not included at all. 
-                // @todo, do not have time to do.
-                
                 $mail = new eZMail();
                 $ini = eZINI::instance();
 				
-				// default content type is text/plain
-				if (isset($contentType) && $contentType == 'text/html') {
-					$mail->setContentType($contentType);
-				}
-                
+                // 2.1 check email sender, because email sender must be specified for each mail.
                 if (!$mail->validate($sender)) {
                     $sender = $ini->variable( "MailSettings", "EmailSender" );
                     if (!$mail->validate($sender)) {
@@ -63,6 +62,9 @@ class bysoftSendTemplateMail
                 }
                 $mail->setSender($sender);
                 
+                // 2.2 setup email attributes
+                
+                // 2.2.1 email receivers
                 if (!is_array($receivers)) {
                     $receivers = array($receivers);
                 }
@@ -73,10 +75,41 @@ class bysoftSendTemplateMail
                     }
                 }
                 
-                $mail->setSubject($subject);
-                $mail->setBody($body);
-                eZMailTransport::send($mail);
+                // 2.2.2 email cc receivers
+                if (isset($ccReceivers)) {
+                    if (!is_array($ccReceivers)) {
+                        $ccReceivers = array($ccReceivers);
+                    }
+                    foreach ($ccReceivers as $c) {
+                        if ($mail->validate($c)) {
+                            $mail->addCc($c);
+                        }
+                    }
+                }
                 
+                // 2.2.3 email bcc receivers
+                if (isset($bccReceivers)) {
+                    if (!is_array($bccReceivers)) {
+                        $bccReceivers = array($bccReceivers);
+                    }
+                    foreach ($bccReceivers as $bc) {
+                        if ($mail->validate($bc)) {
+                            $mail->addBcc($bc);
+                        }
+                    }
+                }
+                
+                // 2.2.4 email subject
+                $mail->setSubject($subject);
+                
+                // 2.2.5 email content type and content
+                // default content type is text/plain
+				if (isset($contentType) && $contentType == 'text/html') {
+					$mail->setContentType($contentType);
+				}
+                $mail->setBody($body);
+                
+                eZMailTransport::send($mail);
                 break;
         }
     }
--- doc/readme.txt
+++ doc/readme.txt
@@ -17,3 +17,23 @@ Date : Septemeber 2010
 
 Eg:
 {send_tpl_mail sender='admin@gmail.com' receivers='cavin.deng@bysoft.fr' subject='hello' body='tests'}
+
+
+4/ complete parameters for this operator
+  
+  Parameter       DataType      Description
+  -------------   ---------     -----------------------------------------------------------------------------------
+  sender          string|null   The sender of this mail, if not present, we will use 'EmailSender' or 'AdminEmail'.
+  receivers       string|array  The recipients 
+  cc_receivers    string|array  The cc recipients
+  bcc_receivers   string|array  The bcc recipients
+  content_type    string|null   The content type, can only be 'text/html', default is 'text/plain'.
+  subject         string        The subject of email
+  body            string        The content
+  
+  
+  
+
+
+
+@todo, think about a way to implement attament, maybe can use ezcMail component
